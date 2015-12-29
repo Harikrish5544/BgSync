c916f37745d838d0dd657b6a7571659a13a3f8fe
unknown <pabbisetty_ha@LP_PABBISETTY_HA.PHOTON>
2015-12-29 15:48:21 +0530

    service worker changed

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 self.addEventListener('sync', function(event) {
      console.log("Inside sync event listner...");
-     console.log("tag value ==",event.registration.tag);
-    if (event.registration.tag == "oneTimeSync") {
+     if (event.registration.tag == "oneTimeSync") {
     	console.log("One Time Sync getting..started");
         console.dir(self.registration);
         console.log("One Time Sync Fired");
